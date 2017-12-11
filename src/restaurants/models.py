8e0fcbbaf405f228b64bdee3667371346ee4162a
wackdot <e0167324@u.nus.edu>
2017-12-11 17:43:59 +0800

    11/12/17 5:44 PM

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -9,7 +9,7 @@ User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class RestaurantLocation(models.Model):
-    owner           = models.ForeignKey(User)
+    owner           = models.ForeignKey(User) #Django Models Unleshed JOINCFE.com
     name            = models.CharField(max_length=120)
     location        = models.CharField(max_length=120, null=True, blank=True)
     category        = models.CharField(max_length=120, null=True, blank=True, validators=[validate_category])
