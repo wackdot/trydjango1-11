7593aa8a43277e3b4c4d7fc36f84037fca94a791
wackdot <e0167324@u.nus.edu>
2017-12-13 22:15:39 +0800

    13/12/17 10:15 PM

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -3,6 +3,9 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 
+from menus.models import Item
+from restaurants.models import RestaurantLocation
+
 User = get_user_model()
 
 class ProfileDetailView(DetailView):
@@ -14,3 +17,15 @@ class ProfileDetailView(DetailView):
             raise Http404
         return get_object_or_404(User, username__iexact=username, is_active=True)
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+
+        user = context['user']
+
+        items_exists = Item.objects.filter(user=user).exists()
+        qs = RestaurantLocation.objects.filter(owner=user)
+
+        if items_exists and qs.exists():
+            context['locations'] = qs
+        return context
