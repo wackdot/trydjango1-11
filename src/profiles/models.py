9931b18244b7c559941972f6a23c06aabe71a0bb
wackdot <e0167324@u.nus.edu>
2017-12-14 22:26:31 +0800

    14/12/17 10:26 PM

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Profile(models.Model):
+    user            = models.OneToOneField(User) #user.profile
+    followers       = models.ManyToManyField(User, related_name='is_following', blank=True)
+    # following       = models.ManyToManyField(User, related_name='following', blank=True)
+    activated       = models.BooleanField(default=False)
+    timestamp       = models.DateTimeField(auto_now_add=True)
+    updated         = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+def post_save_user_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        profile, is_created = Profile.objects.get_or_create(user=instance)
+
+post_save.connect(post_save_user_receiver, sender=User)
